0c093933c10ba168513c586bd51673fcec01d689
fhui28 <fhui28>
2025-09-28 19:38:00 +1000

    upating example in fam_ztpoisson

--- R/fam_ztpoisson.R
+++ R/fam_ztpoisson.R
@@ -52,7 +52,7 @@
 #' 
 #' # Example 2: Generate data from a Poisson GAM and fit a zero-truncated Poisson GAM
 #' dat <- gamSim(1, n = 2000, dist = "poisson", scale = .1)
-#' dat$eta = 0.1*(dat$f0 + dat$f1 + dat$x2 + dat$x3)
+#' dat$eta = 0.1*(dat$f0 + dat$f1 + dat$f2 + dat$f3)
 #' dat$mu <- exp(dat$eta)
 #' dat$y <- rztpois(n = 2000, lambda = dat$mu)
 #' 
@@ -71,7 +71,7 @@
 #' 
 #' err <- abs(fit$fitted.values - exp(fit_gold$fitted.values[1:2000,1]))
 #' summary(err)
-#' # Error here should be small
+#' # Error here should be relatively small
 #' }
 #' 
 
